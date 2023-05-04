4e8547a0d7655df5ad92875e432a69b6bd583501
coffey-dev <scoffey@abc.gob.ar>
2023-05-04 15:49:04 -0300

    Function calculatewinner added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function Board() {
 
   function handleClick(i) {
 
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -22,11 +22,21 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
-const [xIsNext, setXIsNext] = useState(true);
+
+  const [xIsNext, setXIsNext] = useState(true);
 const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -46,3 +56,22 @@ const [squares, setSquares] = useState(Array(9).fill(null));
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
